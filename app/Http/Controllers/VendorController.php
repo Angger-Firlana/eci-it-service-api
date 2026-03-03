5e841969a2f47f71735154b2e6b2853f35cf775e
Angger-Firlana <angger.firlana56@gmail.com>
2026-03-03 14:51:46 +0700

    refactor(user-vendor): call actions directly from controllers

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -3,42 +3,59 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Domains\Vendor\Services\VendorService;
+use App\Domains\Vendor\Actions\CreateVendor;
+use App\Domains\Vendor\Actions\DeleteVendor;
+use App\Domains\Vendor\Actions\GetVendorById;
+use App\Domains\Vendor\Actions\ListVendors;
+use App\Domains\Vendor\Actions\UpdateVendor;
 use App\Http\Requests\Vendor\StoreVendorRequest;
 use App\Http\Requests\Vendor\UpdateVendorRequest;
 
 class VendorController extends Controller
 {
-    //
-    protected $vendorService;
+    protected ListVendors $listVendors;
+    protected CreateVendor $createVendor;
+    protected GetVendorById $getVendorById;
+    protected UpdateVendor $updateVendor;
+    protected DeleteVendor $deleteVendor;
 
-    public function __construct(VendorService $vendorService)
+    public function __construct(
+        ListVendors $listVendors,
+        CreateVendor $createVendor,
+        GetVendorById $getVendorById,
+        UpdateVendor $updateVendor,
+        DeleteVendor $deleteVendor
+    )
     {
-        $this->vendorService = $vendorService;
+        $this->listVendors = $listVendors;
+        $this->createVendor = $createVendor;
+        $this->getVendorById = $getVendorById;
+        $this->updateVendor = $updateVendor;
+        $this->deleteVendor = $deleteVendor;
     }
 
     public function index(Request $request){
-        $vendors = $this->vendorService->getAllVendor($request);
+        $vendors = $this->listVendors->execute($request);
         return response()->json($vendors);
     }
 
     public function store(StoreVendorRequest $request){
-        $vendor = $this->vendorService->createVendor($request->validated());
+        $vendor = $this->createVendor->execute($request->validated());
         return response()->json($vendor, 201);
     }
 
     public function show($id){
-        $vendor = $this->vendorService->getByIdVendor($id);
+        $vendor = $this->getVendorById->execute((int) $id);
         return response()->json($vendor);
     }
 
     public function update($id, UpdateVendorRequest $request){
-        $vendor = $this->vendorService->updateVendor($id, $request->validated());
+        $vendor = $this->updateVendor->execute((int) $id, $request->validated());
         return response()->json($vendor);
     }
 
     public function destroy($id){
-        $this->vendorService->deleteVendor($id);
+        $this->deleteVendor->execute((int) $id);
         return response()->json(null, 204);
     }
 }
