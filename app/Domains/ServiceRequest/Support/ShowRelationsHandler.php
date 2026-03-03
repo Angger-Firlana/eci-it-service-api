7ccfaca1b13f677e42456ac805eb3c12ca4ebfd4
Angger-Firlana <angger.firlana56@gmail.com>
2026-03-03 13:24:24 +0700

    fix device type

--- app/Domains/ServiceRequest/Support/ShowRelationsHandler.php
+++ app/Domains/ServiceRequest/Support/ShowRelationsHandler.php
@@ -10,9 +10,10 @@ class ShowRelationsHandler{
             'user:id,name,email',
             'operator:id,name,email',
             'status:id,name,code',
-            'service_request_details:id,service_request_id,device_id,complaint',
+            'service_request_details:id,service_request_id,device_id,device_type_id,complaint',
             'service_request_details.device:id,device_model_id,serial_number',
-            'service_request_details.device.device_model:id,device_type_id,brand,model'
+            'service_request_details.device.device_model:id,device_type_id,brand,model',
+            'service_request_details.device_type:id,name'
         ];
     }
 
@@ -25,10 +26,11 @@ class ShowRelationsHandler{
             'operator:id,name,email',
             'operator.departments:id,name',
             'status:id,name,code',
-            'service_request_details:id,service_request_id,device_id,complaint,solution',
+            'service_request_details:id,service_request_id,device_id,device_type_id,complaint,solution',
             'service_request_details.device:id,device_model_id,serial_number,bad_asset',
             'service_request_details.device.device_model:id,device_type_id,brand,model',
             'service_request_details.device.device_model.device_type:id,name',
+            'service_request_details.device_type:id,name',
             'service_request_details.complaint_images:id,service_request_detail_id,image_path',
             'vendor_approvals:id,service_request_id,approver_id,assigned_by,assigned_at,approved_at,status_id,notes',
             'vendor_approvals.status:id,name,code',
