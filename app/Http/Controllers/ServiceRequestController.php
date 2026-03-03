06534e024a7a206a98d3a74d61018babeff53543
Angger-Firlana <angger.firlana56@gmail.com>
2026-03-03 14:38:13 +0700

    refactor(service-request): drop pass-through get/delete workflows

--- app/Http/Controllers/ServiceRequestController.php
+++ app/Http/Controllers/ServiceRequestController.php
@@ -7,33 +7,38 @@ use App\Helpers\APIResponse;
 use App\Http\Requests\ServiceRequest\StoreServiceRequest;
 use App\Domains\ServiceRequest\Services\UpdateServiceRequestWorkflow;
 use App\Domains\ServiceRequest\Services\CreateServiceRequestWorkflow;
-use App\Domains\ServiceRequest\Services\GetServiceRequestWorkflow;
-use App\Domains\ServiceRequest\Services\DeleteServiceRequestWorkflow;
+use App\Domains\ServiceRequest\Actions\GetServiceRequest;
+use App\Domains\ServiceRequest\Actions\DeleteServiceRequest;
 use App\Http\Requests\ServiceRequest\UpdateServiceRequest;
 
 class ServiceRequestController extends Controller
 {
-    //
-    protected $deleteServiceRequestWorkflow;
-    protected $updateServiceRequestWorkFlow;
-    protected $createServiceRequestWorkflow;
-    protected $getServiceRequestWorkflow;
-    public function __construct(CreateServiceRequestWorkflow $createServiceRequestWorkflow, UpdateServiceRequestWorkflow $updateServiceRequestWorkFlow, GetServiceRequestWorkflow $getServiceRequestWorkflow, DeleteServiceRequestWorkflow $deleteServiceRequestWorkflow){
-        $this->getServiceRequestWorkflow = $getServiceRequestWorkflow;
+    protected DeleteServiceRequest $deleteServiceRequest;
+    protected UpdateServiceRequestWorkflow $updateServiceRequestWorkFlow;
+    protected CreateServiceRequestWorkflow $createServiceRequestWorkflow;
+    protected GetServiceRequest $getServiceRequest;
+
+    public function __construct(
+        CreateServiceRequestWorkflow $createServiceRequestWorkflow,
+        UpdateServiceRequestWorkflow $updateServiceRequestWorkFlow,
+        GetServiceRequest $getServiceRequest,
+        DeleteServiceRequest $deleteServiceRequest
+    ){
+        $this->getServiceRequest = $getServiceRequest;
         $this->updateServiceRequestWorkFlow = $updateServiceRequestWorkFlow;
         $this->createServiceRequestWorkflow = $createServiceRequestWorkflow;
-        $this->deleteServiceReqeustWorkflow = $deleteServiceRequestWorkflow;
+        $this->deleteServiceRequest = $deleteServiceRequest;
     }
 
     public function index(Request $request){
-        $paginator = $this->getServiceRequestWorkflow->getAllServiceRequest($request);
+        $paginator = $this->getServiceRequest->getAllServiceRequest($request);
         $data = $paginator->items();
         $meta = APIResponse::formatPagination($paginator);
         return APIResponse::success($data, 200, "Success", $meta);
     }
 
     public function show($id){
-        $serviceRequests = $this->getServiceRequestWorkflow->getServiceRequestById($id);
+        $serviceRequests = $this->getServiceRequest->getServiceRequestById((int) $id);
         return APIResponse::success($serviceRequests);
     }
 
@@ -54,7 +59,7 @@ class ServiceRequestController extends Controller
     }
 
     public function destroy($id){
-        $serviceRequests = $this->deleteServiceRequestWorkflow->execute($id);
+        $serviceRequests = $this->deleteServiceRequest->execute((int) $id);
         return APIResponse::success($serviceRequests);
     }
 
@@ -63,7 +68,7 @@ class ServiceRequestController extends Controller
     }
 
     public function stats() {
-        $stats = $this->getServiceRequestWorkflow->getStats();
+        $stats = $this->getServiceRequest->getStats();
         return APIResponse::success($stats);
     }
 }
